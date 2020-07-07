24d2c49e961cb3eccf00c0d0e7ce3e2d1b5ad206
ItsLaro <ivan.reor@gmail.com>
2020-07-07 11:30:34 -0400

    User can now take pictures with their device and attach them to the post

--- app/src/main/java/com/codepath/teleroid/MainActivity.java
+++ app/src/main/java/com/codepath/teleroid/MainActivity.java
@@ -1,24 +1,43 @@
 package com.codepath.teleroid;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.codepath.teleroid.databinding.ActivityMainBinding;
 import com.codepath.teleroid.models.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String TAG = MainActivity.class.getSimpleName(); //logging purposes
+    private static final String TAG = MainActivity.class.getSimpleName(); //logging purposes
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 32; //arbitrary
 
     private ActivityMainBinding binding;
+    private File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +45,170 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View loginView = binding.getRoot();
         setContentView(loginView);
+
+        binding.captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera(view);
+            }
+        });
+
+
+        binding.postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (photoFile == null || binding.newPhoto.getDrawable() == null){
+                    Log.e(TAG, "Attempt to post invalid image");
+                    Toast.makeText(MainActivity.this, "No image selected", Toast.LENGTH_SHORT);
+                    return;
+                }
+                String description = binding.captionField.getText().toString();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(description, currentUser, photoFile);
+            }
+        });
     }
 
-    private void queryPosts(){
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
+        Post post = new Post();
+        post.setCaption(description);
+        post.setImage(new ParseFile(photoFile));
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error in saving: " + e);
+                    Toast.makeText(MainActivity.this, "Posting Error", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Log.i(TAG, "Photo posted successfully");
+
+                //Clean UI
+                binding.captionField.setText("");
+                binding.newPhoto.setImageResource(0);
+            }
+        });
+
+    }
+
+    private void queryPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
-                if (e != null){
+                if (e != null) {
                     Log.e(TAG, "Couldn't query posts: " + e);
                     return;
                 }
-                for(Post post: posts) {
+                for (Post post : posts) {
                     Log.i(TAG, "Post retrieved successfully: '" + post.getCaption()
                             + "', by username: " + post.getUser().getUsername());
                 }
             }
         });
     }
+
+    /**
+     * Implicit intent to launch camera application.
+     * Outputs taken picture into fileProvider.
+     */
+    public void launchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        String photoFileName = "photo.jpg";
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+        else {
+            Log.e(TAG, "No available camera app");
+        }
+    }
+
+    /**
+     * Helper method to get the identifier of the captured image.
+     * @Return the File for a photo stored on disk given the fileName
+     */
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        String pathName = mediaStorageDir.getPath() + File.separator + fileName;
+        File file = new File(pathName);
+
+        return file;
+    }
+
+    /**
+     * Method that corrects the orientation of the picture based on the EXIF data
+     * from when the picture was taken and the orientation of the device.
+     * @param photoFilePath
+     * @return Bitmap
+     */
+    public Bitmap rotateBitmapOrientation(String photoFilePath) {
+        // Create and configure BitmapFactory
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoFilePath, bounds);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
+        // Read EXIF Data
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(photoFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+        // Rotate Bitmap
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+        // Return result
+        return rotatedBitmap;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Retrieve from disk and perform rotation
+                Bitmap takenImage = rotateBitmapOrientation(photoFile.getAbsolutePath());
+
+                // Load the taken image into a preview
+                binding.newPhoto.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Log.i(TAG, "Picture wasn't taken!");
+            }
+        }
+    }
 }
+
+
